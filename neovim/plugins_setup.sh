ecae60018e5f51b6bb7f30d29c94b1eabf60c08c
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-08 19:20:30 +0200

    Enhance neovim plugin setup script
    Updated plugin installation script to include additional plugins and corrected comments.

--- neovim/plugins_setup.sh
+++ neovim/plugins_setup.sh
@@ -13,12 +13,18 @@ mkdir -p ~/.config/nvim/lua/config
 # download your init
 curl -sL -o ~/.config/nvim/init.lua https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/neovim/init.lua
 
-# install plugin manager
+# install Plugin Manager
 curl -sL -o ~/.config/nvim/lua/config/lazy.lua https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/neovim/lua/config/lazy.lua
 
-# setup yazi
-curl -sL -o ~/.config/nvim/lua/plugins/yazi.lua https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/neovim/lua/plugins/yazi.lua
+# install Language Server Manager
+curl -sL -o ~/.config/nvim/lua/plugins/mason.lua "$BASEURL/neovim/lua/plugins/mason.lua"
 
+# install plugins
+curl -sL -o ~/.config/nvim/lua/plugins/treesitter.lua "$BASEURL/neovim/lua/plugins/treesitter.lua"
+curl -sL -o ~/.config/nvim/lua/plugins/lspconfig.lua "$BASEURL/neovim/lua/plugins/lspconfig.lua"
+curl -sL -o ~/.config/nvim/lua/plugins/telescope.lua "$BASEURL/neovim/lua/plugins/telescope.lua"
+curl -sL -o ~/.config/nvim/lua/plugins/yazi.lua "$BASEURL/neovim/lua/plugins/yazi.lua"
+curl -sL -o ~/.config/nvim/lua/plugins/mini-pairs.lua "$BASEURL/neovim/lua/plugins/mini-pairs.lua"
 
 # done
 echo "Setup completed. now open neovim with 'nvim' and follow instructions."
