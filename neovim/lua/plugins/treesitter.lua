78526ecdd77ec4d21fe52f35ba885c0f4959e34f
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-08 19:13:40 +0200

    Add bash, html, and css to ensure_installed

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "python", "cpp", "javascript", "typescript", "markdown", "latex", "lua" },
+      ensure_installed = { "python", "cpp", "javascript", "typescript", "markdown", "latex", "lua", "bash", "html", "css" },
       highlight = { enable = true },
     })
   end,
