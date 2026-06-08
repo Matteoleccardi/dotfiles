8a39478d074376d7bc9a78d0dc8161ffd924c15f
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-08 19:07:54 +0200

    Add Mason plugin configuration for Neovim

---
+++ neovim/lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = { "williamboman/mason-lspconfig.nvim" },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      -- Auto-downloads language servers for Python, C++, JS/TS, and LaTeX
+      ensure_installed = { "pyright", "clangd", "ts_ls", "texlab" },
+    })
+  end,
+}
