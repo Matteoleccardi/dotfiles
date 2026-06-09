1aeb85bcd538cb2afdd4ae29bfff2fe1e813926a
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-09 11:44:02 +0200

    Modify Treesitter highlight configuration
    Updated highlight configuration to include additional_vim_regex_highlighting option.

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -5,7 +5,10 @@ return {
     -- CHANGED: 'config' instead of 'configs' to match modern nvim-treesitter
     require("nvim-treesitter.config").setup({
       ensure_installed = { "python", "cpp", "javascript", "typescript", "markdown", "latex", "lua", "bash" },
-      highlight = { enable = true },
+      highlight = {
+        enable = true,              -- False will disable the whole extension
+        additional_vim_regex_highlighting = false,
+      },
     })
   end,
 }
