50b1f076217b168560ae8fcc63e9f7474c6720b5
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-09 19:42:59 +0200

    Update update_configs.sh with theme instructions
    Add comments for theme retrieval from GitHub.

--- yazi/update_configs.sh
+++ yazi/update_configs.sh
@@ -1,6 +1,10 @@
 mkdir -p ~/.config/yazi
 cd ~/.config/yazi
 
+# get theme from github catpuccin color theme (mocha-peach)
+# (https://github.com/catppuccin/yazi/tree/main/themes/mocha)
+# curl -sL -o ./theme.toml https://raw.githubusercontent.com/catppuccin/yazi/refs/heads/main/themes/mocha/catppuccin-mocha-peach.toml
+
 for f in yazi theme keymap vfs; do
   curl -sLO "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/yazi/$f.toml"
 done
