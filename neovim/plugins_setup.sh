6df6fb3e1feb151038f50347079e4cd7e69c4a64
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-09 12:05:46 +0200

    Update plugins_setup.sh

--- neovim/plugins_setup.sh
+++ neovim/plugins_setup.sh
@@ -11,22 +11,22 @@ mkdir -p ~/.config/nvim/lua/plugins
 mkdir -p ~/.config/nvim/lua/config
 
 BASEURL="https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main"
+BASECONFIG="$BASEURL/neovim/lua/config"
+BASEPLUGINS="$BASEURL/neovim/lua/plugins"
 
 # download your init
 curl -sL -o ~/.config/nvim/init.lua "$BASEURL/neovim/init.lua"
 
 # install Plugin Manager
-curl -sL -o ~/.config/nvim/lua/config/lazy.lua "$BASEURL/neovim/lua/config/lazy.lua"
+curl -sL -o ~/.config/nvim/lua/config/lazy.lua "$BASECONFIG/lazy.lua"
 
 # install Language Server Manager
-curl -sL -o ~/.config/nvim/lua/plugins/mason.lua "$BASEURL/neovim/lua/plugins/mason.lua"
+curl -sL -o ~/.config/nvim/lua/plugins/mason.lua "$BASEPLUGINS/mason.lua"
 
 # install plugins
-curl -sL -o ~/.config/nvim/lua/plugins/treesitter.lua "$BASEURL/neovim/lua/plugins/treesitter.lua"
-curl -sL -o ~/.config/nvim/lua/plugins/lspconfig.lua "$BASEURL/neovim/lua/plugins/lspconfig.lua"
-curl -sL -o ~/.config/nvim/lua/plugins/telescope.lua "$BASEURL/neovim/lua/plugins/telescope.lua"
-curl -sL -o ~/.config/nvim/lua/plugins/yazi.lua "$BASEURL/neovim/lua/plugins/yazi.lua"
-curl -sL -o ~/.config/nvim/lua/plugins/mini-pairs.lua "$BASEURL/neovim/lua/plugins/mini-pairs.lua"
+for plugin in treesitter lspconfig telescope yazi mini-pairs tokionight; do
+    curl -sL -o ~/.config/nvim/lua/plugins/${plugin}.lua "$BASEPLUGINS/${plugin}.lua" || echo "Failed: ${plugin}.lua"
+done
 
 # done
 echo "Setup completed. now open neovim with 'nvim' and follow instructions."
