3ad6e138f1cedb1e4ed041c4279f0b79dba3c9e6
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-09 19:34:52 +0200

    Update update_configs.sh

--- yazi/update_configs.sh
+++ yazi/update_configs.sh
@@ -1,4 +1,6 @@
-cd ~/.config/yazi || exit 1
+mkdir -p ~/.config/yazi
+cd ~/.config/yazi
 
 for f in yazi theme keymap vfs; do
   curl -sLO "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/yazi/$f.toml"
+done
