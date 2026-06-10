76b729b9d4ea26f987d0f762dd291ccc7828f09a
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-10 14:16:13 +0200

    Update setup.sh

--- fonts/setup.sh
+++ fonts/setup.sh
@@ -3,14 +3,25 @@
 # Setup fonts downloading on linux.
 
 # NerdFonts
-# https://github.com
+# https://github.com/ryanoasis/nerd-fonts/
 mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts
+cd ~/.local/share/fonts || exit 1
 
-for f in Hack FiraCode FiraMono UbuntuMono JetBrainsMono; do
-  curl -sLO "https://github.comreleases/download/v3.4.0/$f.zip"
-  unzip "$f.zip" -d ~/.local/share/fonts/
+for f in FiraMono Mononoki NerdFontsSymbolsOnly UbuntuMono; do
+  curl -sLO "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/$f.zip"
+  mkdir -p "$f"
+  unzip -qo "$f.zip" -d "$f/"
   rm "$f.zip"
 done
 
 fc-cache -fv
+
+echo "Check installation:"
+fc-list : family | grep -i "fira"
+fc-list : family | grep -i "mononoki"
+fc-list : family | grep -i "symbol"
+fc-list : family | grep -i "ubuntu"
+
+
+
+
