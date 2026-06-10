b97d7ae4c4d136cd26d6e992114bb85c4f0e81d6
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-10 12:17:34 +0200

    Update update_tmux.conf.sh

--- tmux/update_tmux.conf.sh
+++ tmux/update_tmux.conf.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
+# download the current .timux.conf file
 cd ~
 curl -sL -o "./.tmux.conf" https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/tmux/.tmux.conf
+
+# install catpuccin stuyle plugin
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+# all styles specifications are already in the .tmux.conf file, no need to do anything else here
