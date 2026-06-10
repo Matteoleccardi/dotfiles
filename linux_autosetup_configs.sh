0ecc9dd1bc484515d8e66c8a1b55350f9761fc85
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-10 17:47:21 +0200

    Add linux_autosetup_configs.sh for config updates
    This script updates various configuration files and installs necessary packages from a GitHub repository.

---
+++ linux_autosetup_configs.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+echo "Updating all configs from"
+echo "https://github.com/Matteoleccardi/dotfiles/"
+
+MYCURRDIR="$PWD"
+
+# vim
+cd
+curl -sLO "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/Vim/.vimrc"
+
+# bashrc.d
+cd
+mkdir -p ./.bashrc.d
+curl -sL -o ./__upd.sh "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/bashrc.d/update.sh"
+bash ./__upd.sh
+echo "If it is the first time on this machine updating the bashrc.d, then have a look here, second section:"
+echo "https://github.com/Matteoleccardi/dotfiles/tree/main/bashrc.d"
+
+# fonts
+cd
+curl -sL -o ./__upd.sh "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/fonts/setup.sh"
+bash ./__upd.sh
+
+# nvim
+cd
+curl -sL -o ./__upd.sh "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/neovim/plugins_setup.sh"
+bash ./__upd.sh
+
+# tmux
+cd
+curl -sL -o ./__upd.sh "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/tmux/update_tmux.conf.sh"
+bash ./__upd.sh
+
+# yazi
+cd
+curl -sL -o ./__upd.sh "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/yazi/update_configs.sh"
+bash ./__upd.sh
+
+# condardc
+echo "check out your ~/.condarc file. I cannot update it manually, but you can look at this:"
+echo "https://github.com/Matteoleccardi/dotfiles/blob/main/miniconda3/.condarc"
+
+
+
+
+
+# list of packages to install manually
+echo "Please install the following yourself:"
+echo "yazi zoxide fzf tmux screen vim neovim miniconda 7z"
+echo "htop btop fastfetch ffmpeg  mpv"
+
+
+# cleanup
+cd
+rm -f ./__upd.sh
+
+cd "$MYCURRDIR"
