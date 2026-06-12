cb259e4e5b071c4eb6a96582da429fbc7341f615
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-12 18:57:36 +0200

    Add 'fzf' to the update script file list

--- bashrc.d/update.sh
+++ bashrc.d/update.sh
@@ -1,5 +1,5 @@
 cd ~/.bashrc.d/
-for f in aliases aliases-ethz custom paths paths-ethz yazi zoxide; do
+for f in aliases aliases-ethz custom fzf paths paths-ethz yazi zoxide; do
   curl -sL -O  "https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/bashrc.d/$f" || echo "Falied: $f"
 done
 cd
