445e9f808eab9c963f42d873be378c1badf5bc0c
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-15 14:55:30 +0200

    Modify setup.sh comments and add curl command
    Updated comments for clarity and added curl command.

--- mdp/setup.sh
+++ mdp/setup.sh
@@ -1,6 +1,9 @@
 cd
-# create playlists dir if not exists
+# create dirs if not exist
 mkdir -p ~/.config/mpd/playlists
+
+# cd ~/.config/mpd
+curl -sLO 
 
 # register service to start automatically at every startup
 systemctl --user enable --now mpd
