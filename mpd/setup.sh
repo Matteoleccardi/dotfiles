55b5ad90b7ff050eafb4f8102a0578c5e0c6ebfb
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-15 15:14:20 +0200

    Update MPD setup instructions for user mode

--- mpd/setup.sh
+++ mpd/setup.sh
@@ -10,5 +10,5 @@ curl -sLO https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/m
 echo "[ACTION REQUIRED] Go to the conf file and change the default music folder(s):"
 echo "~/.config/mpd/mpd.conf"
 
-echo "[START IT] To start it, run the following command:"
-echo "sudo systemctl enable --now mpd"
+echo "[START IT] To start it, and enable autostart when boot up, run the following command:"
+echo "systemctl --user enable --now mpd # WITHOUT sudo"
