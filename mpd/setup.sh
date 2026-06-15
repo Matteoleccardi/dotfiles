8b728d446942296992311dd2d0ad98302815ca02
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-15 15:05:52 +0200

    Update setup.sh

--- mpd/setup.sh
+++ mpd/setup.sh
@@ -7,8 +7,8 @@ mkdir -p ~/.config/mpd/playlists
 cd ~/.config/mpd
 curl -sLO https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/mpd/mpd.conf
 
-# register service to start automatically at every startup
-systemctl --user enable --now mpd
-
 echo "[ACTION REQUIRED] Go to the conf file and change the default music folder(s):"
 echo "~/.config/mpd/mpd.conf"
+
+echo "[START IT] To start it, run the following command:"
+echo "sudo systemctl enable --now mpd"
