378032b6b4ea87c976a03dc2a398b3e225d7cbc5
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-15 15:01:16 +0200

    Update setup.sh

--- mpd/setup.sh
+++ mpd/setup.sh
@@ -1,1 +1,14 @@
+cd
 
+# create dirs if not exist
+mkdir -p ~/.config/mpd/playlists
+
+# download the conf file
+cd ~/.config/mpd
+curl -sLO https://raw.githubusercontent.com/Matteoleccardi/dotfiles/refs/heads/main/mpd/mpd.conf
+
+# register service to start automatically at every startup
+systemctl --user enable --now mpd
+
+echo "[ACTION REQUIRED] Go to the conf file and change the default music folder(s):"
+echo "~/.config/mpd/mpd.conf"
