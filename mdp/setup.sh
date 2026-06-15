f338e15e38c9a4e4c47e26bbaaff6af6702e415c
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-15 15:01:31 +0200

    Delete mdp directory

--- mdp/setup.sh
+++
@@ -1,9 +0,0 @@
-cd
-# create dirs if not exist
-mkdir -p ~/.config/mpd/playlists
-
-# cd ~/.config/mpd
-curl -sLO 
-
-# register service to start automatically at every startup
-systemctl --user enable --now mpd
