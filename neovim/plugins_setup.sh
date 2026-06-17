7caae1199dea539dab20241adc38061bee05a825
Matteo Leccardi <leccardi.matteo@gmail.com>
2026-06-17 19:31:34 +0200

    Update plugins_setup.sh

--- neovim/plugins_setup.sh
+++ neovim/plugins_setup.sh
@@ -20,9 +20,6 @@ curl -sL -o ~/.config/nvim/init.lua "$BASEURL/neovim/init.lua"
 # install Plugin Manager
 curl -sL -o ~/.config/nvim/lua/config/lazy.lua "$BASECONFIG/lazy.lua"
 
-# install Language Server Manager
-curl -sL -o ~/.config/nvim/lua/plugins/mason.lua "$BASEPLUGINS/mason.lua"
-
 # install plugins
 # (
 #   this might be useful:
@@ -30,7 +27,7 @@ curl -sL -o ~/.config/nvim/lua/plugins/mason.lua "$BASEPLUGINS/mason.lua"
 #   cd ~/.config/nvim/lua/plugins/
 #   a=""; for f in *; do a="$a ${f%.*}"; done; echo $a; unset a;
 # )
-for plugin in catppuccin lspconfig lualine mason mini-pairs telescope tokionight treesitter undotree yazi; do
+for plugin in alpha catppuccin completions lspconfig lualine mason mini-pairs none-ls telescope tokionight treesitter undotree yazi; do
     curl -sL -o ~/.config/nvim/lua/plugins/${plugin}.lua "$BASEPLUGINS/${plugin}.lua" || echo "Failed: ${plugin}.lua"
 done
 
